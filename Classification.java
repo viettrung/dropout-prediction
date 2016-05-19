561f56828e315801fdd1d810e0fda8c5a04c9719
LưuVN <luuvn91@gmail.com>
2016-05-19 08:48:11 +0700

    naive bayes implement

---
+++ Classification.java
@@ -0,0 +1,60 @@
+import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.Evaluation;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Discretize;
+
+public class Classification {
+
+	public static void main(String[] args) throws Exception {
+		DataSource source = new DataSource("Y1_formated.arff");
+		Instances data = source.getDataSet();
+		// setting class attribute if the data format does not provide this
+		// information
+		if (data.classIndex() == -1)
+			data.setClassIndex(4);
+
+		// filter
+		// Remove rm = new Remove();
+		// rm.setAttributeIndices("1"); // remove 1st attribute: roll number
+		// remove.setInputFormat(data);
+		// data = Filter.useFilter(data, rm);
+
+		int folds = 10;
+		String[] options = new String[6];
+		options[0] = "-B";
+		options[1] = "" + folds;
+		options[2] = "-M";
+		options[3] = "-1.0";
+		options[4] = "-R";
+		options[5] = "first-last";
+
+		Discretize filter = new Discretize();
+		filter.setOptions(options);
+		filter.setInputFormat(data);
+
+		Instances newData = Filter.useFilter(data, filter);
+
+		// perform cross-validation
+		Evaluation eval = new Evaluation(newData);
+		for (int n = 0; n < folds; n++) {
+			Instances train = newData.trainCV(folds, n);
+			Instances test = newData.testCV(folds, n);
+
+			// build and evaluate classifier
+			Classifier clsNaive = new NaiveBayes();
+			clsNaive.buildClassifier(train);
+			eval.evaluateModel(clsNaive, test);
+		}
+
+		// output evaluation
+		System.out.println();
+		System.out.println("=== Setup ===");
+		System.out.println("Dataset: " + data.relationName());
+		System.out.println(eval.toSummaryString());
+		System.out.println(eval.toClassDetailsString());
+		System.out.println(eval.toMatrixString());
+	}
+}
